73930dcd8584621b2b3fcdeb6268675f2bb4a488
Kyrtsun Oleh <olegkirtsun@gmail.com>
2023-12-05 08:01:08 +0200

    finally changes

--- Data_base/database.py
+++ Data_base/database.py
@@ -40,20 +40,6 @@ def sql_start():
             logger.info('Таблица продукты создана')
         except Exception as e:
             logger.critical(f'Таблица продукты не была созадана - {e}')
-    # with conn:
-    #     try:
-    #         cur.execute(
-    #             'CREATE TABLE IF NOT EXISTS products ('
-    #             'product_id SERIAL PRIMARY KEY,'
-    #             'name TEXT NOT NULL,'
-    #             'size INTEGER NOT NULL,'
-    #             'sm FLOAT NOT NULL,'
-    #             'condition INTEGER NOT NULL,'
-    #             'price FLOAT NOT NULL)'
-    #         )
-    #         logger.info('Таблица продукты создана')
-    #     except Exception as e:
-    #         logger.critical(f'Таблица продукты не была созадана - {e}')
         try:
             cur.execute(
                 'CREATE TABLE IF NOT EXISTS users ('
@@ -65,34 +51,10 @@ def sql_start():
         except Exception as e:
             logger.critical(f'Таблица юзеры не была создана - {e}')
 
-        # try:
-        #     cur.execute(
-        #         'CREATE TABLE IF NOT EXISTS photo ('
-        #         'id SERIAL PRIMARY KEY,'
-        #         'photo TEXT,'
-        #         'product_id INTEGER REFERENCES products(product_id) ON DELETE CASCADE)')
-        #     logger.info('Таблица Фото создана')
-        #     conn.commit()
-        # except Exception as e:
-        #     logger.critical(f'Таблица Фото не была создана - {e}')
-        #
-        # try:
-        #     cur.execute(
-        #         'CREATE TABLE IF NOT EXISTS video ('
-        #         'id SERIAL PRIMARY KEY,'
-        #         'video TEXT,'
-        #         'product_id INTEGER REFERENCES products(product_id) ON DELETE CASCADE)')
-        #     logger.info('Таблица Видео создана')
-        #     conn.commit()
-        # except Exception as e:
-        #     logger.critical(f'Таблица Видео не была создана - {e}')
-
 
 async def save_post(data):
     videos = []
     photos = []
-    # query_photo = 'INSERT INTO photo (photo, product_id) VALUES (%s, %s)'
-    # query_video = 'INSERT INTO video (video, product_id) VALUES (%s, %s)'
     info = {}
     for key, value in data.items():
         if key.startswith('photo'):
@@ -104,36 +66,6 @@ async def save_post(data):
 
     photo_json = json.dumps(photos)
     videos_json = json.dumps(videos)
-
-    # with conn:
-    #     try:
-    #         cur.execute(
-    #             'INSERT INTO products (name, size, sm, condition, price) '
-    #             'VALUES (%s, %s, %s, %s, %s) '
-    #             'RETURNING product_id',
-    #             (data.get('name'), data.get('size'), data.get('sm'),
-    #              data.get('condition'), data.get('price'))
-    #         )
-    #         product_id = cur.fetchone()[0]
-    #         logger.info(f'Данные вставлены в таблицу "products"')
-    #     except Exception as e:
-    #         logger.critical(f'Ошибка при вставке данных в таблицу "products": {e}')
-    #
-    # for key, value in data.items():
-    #     if key.startswith('photo'):
-    #         photos.append((value, product_id))
-    #     elif key.startswith('video'):
-    #         videos.append((value, product_id))
-    #
-    # with conn:
-    #     try:
-    #         cur.executemany(query_photo, photos)
-    #         cur.executemany(query_video, videos)
-    #         logger.info('Данные вставлены в таблицы фото и видео!')
-    #         return True
-    #     except Exception as e:
-    #         logger.critical(f'Данные не сохраненты! {e}')
-    #         return False
 
     with conn:
         try:
@@ -179,19 +111,6 @@ async def save_user_info(name, user_id):
 async def get_size(size, sm):
     with conn:
         try:
-        #     cur.execute('SELECT products.product_id,'
-        #                 'products.name,'
-        #                 'products.size,'
-        #                 'products.sm,'
-        #                 'products.condition,'
-        #                 'products.price,'
-        #                 'photo.photo,'
-        #                 'video.video '
-        #                 'FROM products '
-        #                 'LEFT JOIN photo ON products.product_id = photo.product_id '
-        #                 'LEFT JOIN video ON products.product_id = video.product_id '
-        #                 'WHERE products.size = %s AND products.sm =%s', (size, sm))
-
             cur.execute('SELECT * FROM products WHERE size=%s and sm=%s', (size, sm))
             res = cur.fetchall()
 
